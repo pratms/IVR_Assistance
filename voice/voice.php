22aecd6229e1e6da4ac76daf369413c114ae7fc1
Pratik modak <pratikmodak@icloud.com>
2016-12-02 17:56:23 -0500

    Update voice.php

--- voice/voice.php
+++ voice/voice.php
@@ -1,9 +1,12 @@
-<Response>
-<Say>
-Welcome to Twilio. Congratulations, you have successfully configured your development environment.
-</Say>
-<Play>/sounds/clapping.wav</Play>
-<Say>
-Have a fun time with your development! Goodbye for now.
-</Say>
-</Response>
+<?php
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<Response>';
+echo '<Say>';
+echo 'Welcome to Twilio. Congratulations, you have successfully configured your development environment.'
+echo '</Say>';
+echo '<Play>/sounds/clapping.wav</Play>';
+echo '<Say>';
+echo 'Have a fun time with your development! Goodbye for now.';
+echo '</Say>';
+echo '</Response>';
+?>
