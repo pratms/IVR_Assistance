8e6ed05b503b9011825e20e2f8e58a6eb3da9b49
Pratik modak <pratikmodak@icloud.com>
2016-12-02 19:40:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,175 +28,49 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.post('/view1', function(req, res) {
-    console.log(req.body.search);
-    res.end();
-    var accountSid = 'AC5b3a64ad844dfbb918812897bcf2a1ce'; 
-    var authToken = '8c055fe15f07533ff69388be72b93b16';  
+app.post('/', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  let twiml = new twilio.TwimlResponse();
 
-    var twilio = require('twilio');
-    var client = new twilio.RestClient(accountSid, authToken);
-if (req.body.search) 
-{
-  var num = req.body.search;
-
-
-client.calls.create({
-    url: 'http://' + request.headers.host + '/voice',
-    to: num,
-    from: "+16466528019 "
-}, function(err, call) {
-    process.stdout.write(call.sid);
-});
-
-};
-});
-
-
-app.get('/posts', function(req, res)
-{
-   db.details.find(function(err, data) 
-  {
-
-     console.log(data);
-     res.json(data);
+  twiml.gather({ 
+    numDigits: 1,
+    action: '/gather'
+  }, (gatherNode) => {
+    gatherNode.say('For courses, press 1. For course grades, press 2.');
   });
 
+  // If the user doesn't enter input, loop
+  twiml.redirect('/voice');
 
-
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
 });
 
+// Create a route that will handle <Gather> input
+app.post('/gather', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  let twiml = new twilio.TwimlResponse();
 
-app.get('/', function(req, res) {
-    var twilio = require('twilio');
-    var twiml = new twilio.TwimlResponse();
-
-exports.voice = function(request, response) {
-    var twiml = new twilio.TwimlResponse();
- 
-    twiml.say('Hi there! Thanks for calling to wish Joe good luck this season. Please leave your message after the beep - you can end the message by pressing any button. Get ready!')
-        .record({
-            maxLength:120,
-            action:'/recording'
-        });
- 
-    response.send(twiml);
-};
-       if (req.body.Body == 'Help') {
-      twiml.say("Hello from pratik Twilio! Have fun.");
-     }
-     // else
-     // {
-   //    var string = req.body.Body;
-   //      var from = req.body.From;
-
-   //    string = string.split(" ");
-   //    var stringArray = new Array();
-   //    for(var i =0; i < string.length; i++)
-   //    {
-   //    stringArray.push(string[i]);
-
-   //    }
-   //    var str = stringArray[1].toLowerCase();
-
-   //    var zip = stringArray[0];
-
-   // if (str == 'a') {
-   //          twiml.message('Thanks for your feedback');
-            
-   //        db.details.insert( { number: from, zip: zip, response: "Extermely Satisfied"  } )
-
-   //  } 
-    
-   //  else if(str == 'b') {
-   //      twiml.message('Thanks for your feedback');
-        
-   //      db.details.insert( { number: from, zip: zip, response: "Moderately Satisfied"  } )
-    
-   //   } 
-   //   else if(str == 'c') 
-   //   {
-
-   //      twiml.message('Thanks for your feedback');
-        
-   //      db.details.insert( { number: from, zip: zip, response: "Not at all Satisfied"  } )
-     // }
-
-     else 
-     {
-        twiml.message('Invalid Response try again.');
+  // If the user entered digits, process their request
+  if (request.body.Digits) {
+    switch (request.body.Digits) {
+      case '1': twiml.say('You selected courses. Good for you!'); break;
+      case '2': twiml.say('You need see grades?'); break;
+      default: 
+        twiml.say('Sorry, I don\'t understand that choice.').pause();
+        twiml.redirect('/voice');
+        break;
     }
+  } else {
+    // If no input was sent, redirect to the /voice route
+    twiml.redirect('/voice');
+  }
 
-
-
-     
-
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
 });
-
-app.post('/', function(req, res) {
-    var twilio = require('twilio');
-    var twiml = new twilio.TwimlResponse();
-    twiml.say("Hello from pratik Twilio! Have fun.");
-
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-    var twiml = new twilio.TwimlResponse();
-       if (req.body.Body == 'Help') {
-      twiml.say("Hello from pratik Twilio! Have fun.");
-     }
-   //   else
-   //   {
-   //    var string = req.body.Body;
-   //      var from = req.body.From;
-
-   //    string = string.split(" ");
-   //    var stringArray = new Array();
-   //    for(var i =0; i < string.length; i++)
-   //    {
-   //    stringArray.push(string[i]);
-
-   //    }
-   //    var str = stringArray[1].toLowerCase();
-
-   //    var zip = stringArray[0];
-
-   // if (str == 'a') {
-   //          twiml.message('Thanks for your feedback');
-            
-   //        db.details.insert( { number: from, zip: zip, response: "Extermely Satisfied"  } )
-
-   //  } 
-    
-   //  else if(str == 'b') {
-   //      twiml.message('Thanks for your feedback');
-        
-   //      db.details.insert( { number: from, zip: zip, response: "Moderately Satisfied"  } )
-    
-   //   } 
-   //   else if(str == 'c') 
-   //   {
-
-   //      twiml.message('Thanks for your feedback');
-        
-   //      db.details.insert( { number: from, zip: zip, response: "Not at all Satisfied"  } )
-   //   }
-
-     else 
-     {
-        twiml.message('Invalid Response try again.');
-    }
-
-
-
-
-        
- 
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-});
-
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
