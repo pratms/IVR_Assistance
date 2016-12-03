e6094034799fa3148699de155ef3212b5b9b7570
Pratik modak <pratikmodak@icloud.com>
2016-12-02 19:55:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -134,51 +134,8 @@ exports.voice = function(request, response) {
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 });
-app.post('/voice', (request, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
-  let twiml = new twilio.TwimlResponse();
 
-  twiml.gather({ 
-    numDigits: 1,
-    action: '/gather'
-  }, (gatherNode) => {
-    gatherNode.say('For sales, press 1. For support, press 2.');
-  });
-
-  // If the user doesn't enter input, loop
-  twiml.redirect('/voice');
-
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
-});
-
-// Create a route that will handle <Gather> input
-app.post('/gather', (request, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
-  let twiml = new twilio.TwimlResponse();
-
-  // If the user entered digits, process their request
-  if (request.body.Digits) {
-    switch (request.body.Digits) {
-      case '1': twiml.say('You selected sales. Good for you!'); break;
-      case '2': twiml.say('You need support. We will help!'); break;
-      default: 
-        twiml.say('Sorry, I don\'t understand that choice.').pause();
-        twiml.redirect('/voice');
-        break;
-    }
-  } else {
-    // If no input was sent, redirect to the /voice route
-    twiml.redirect('/voice');
-  }
-
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
-});
-
-app.post('/voice', function(req, res) {
+app.post('/', function(req, res) {
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
     twiml.say("Hello from pratik Twilio! Have fun.");
@@ -220,5 +177,6 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
 
 
