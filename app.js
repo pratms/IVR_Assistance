8fa961dce92eb3769b3e6664f0d75fb8d52b6b83
Pratik modak <pratikmodak@icloud.com>
2016-12-02 20:21:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -138,14 +138,11 @@ exports.voice = function(request, response) {
 app.post('/voice', function(req, res) {
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
-  function gather() {
+ 
     twiml.gather({ numDigits: 1 }, (gatherNode) => {
       gatherNode.say('For sales, press 1. For support, press 2.');
     });
     
-    // If the user doesn't enter input, loop
-    twiml.redirect('/voice');
-  }
 
   // If the user entered digits, process their request
   if (request.body.Digits) {
@@ -154,12 +151,12 @@ app.post('/voice', function(req, res) {
       case '2': twiml.say('You need support. We will help!'); break;
       default: 
         twiml.say('Sorry, I don\'t understand that choice.').pause();
-        gather();
+       
         break;
     }
   } else {
     // If no input was sent, use the <Gather> verb to collect user input 
-    gather();
+
   }
 
   // Render the response as XML in reply to the webhook request
