edfd823c0fbab45c1c4f2f5f71f41809348fe0b4
Pratik modak <pratikmodak@icloud.com>
2016-12-02 20:33:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(urlencoded({ extended: false }));
 app.post('/voice', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   let twiml = new twilio.TwimlResponse();
-
-twiml.gather({ 
+  twiml.gather({ 
     numDigits: 1,
     action: '/gather'
   }, (gatherNode) => {
@@ -34,35 +33,6 @@ twiml.gather({
 app.post('/gather', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   let twiml = new twilio.TwimlResponse();
-
-  // If the user entered digits, process their request
- twiml.gather({ 
-    numDigits: 1,
-    action: '/gather'
-  }, (gatherNode) => {
-    gatherNode.say('For sales, press 1. For support, press 2.');
-  });
-
-  // If the user doesn't enter input, loop
-  twiml.redirect('/voice');
-
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
-});
-
-// Create a route that will handle <Gather> input
-app.post('/gather', (request, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
-  let twiml = new twilio.TwimlResponse();
- function gather() {
-    twiml.gather({ numDigits: 1 }, (gatherNode) => {
-      gatherNode.say('For sales, press 1. For support, press 2.');
-    });
-    
-    // If the user doesn't enter input, loop
-    twiml.redirect('/voice');
-  }
 
   // If the user entered digits, process their request
   if (request.body.Digits) {
@@ -71,19 +41,18 @@ app.post('/gather', (request, response) => {
       case '2': twiml.say('You need support. We will help!'); break;
       default: 
         twiml.say('Sorry, I don\'t understand that choice.').pause();
-        gather();
+        twiml.redirect('/voice');
         break;
     }
   } else {
-    // If no input was sent, use the <Gather> verb to collect user input 
-    gather();
+    // If no input was sent, redirect to the /voice route
+    twiml.redirect('/voice');
   }
 
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
 });
-
 // Create an HTTP server and listen for requests on port 3000
 app.listen(3000);
 
