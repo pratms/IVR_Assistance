76f5406cb3da163f712147d54cf3a2409f6a679d
Pratik modak <pratikmodak@icloud.com>
2016-12-15 12:55:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -23,28 +23,32 @@ app.post('/voice', (request, response) =>
   (gatherNode) => {
     gatherNode.say(' Please enter your 5 digit ID and then press star.');
   });
+
+  // If the user doesn't enter input, loop
   twiml.redirect('/voice');
+
+  // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
 });
 
+// Create a route that will handle <Gather> input
 app.post('/gather', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
    let twiml = new twilio.TwimlResponse();
    if (request.body.Digits) {
    db.users.findOne({userid: request.body.Digits},function(err, data) 
   {
     if (data) {
-    twiml.say('wellcome help desk' data.name); 
-
-    };
-    else{
+    twiml.say('wellcome %s to help desk', data.name); 
+    }
+   else{
         twiml.say('Sorry, invalid userid').pause();
         twiml.redirect('/voice');
 
     }
      
- 
-  });
+   });
 
   
     // switch (request.body.Digits) {
@@ -69,6 +73,9 @@ app.post('/gather', (request, response) => {
 app.listen(3000);
 
 module.exports = app;
+
+
+
 
 
 
